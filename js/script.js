e3cd64768e3541e2c4a7a0e1f39c0e7124ae55ed
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-01-09 11:27:46 +0100

    basic structure of navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
+
+
+
+    //  toggle nav-links when press nav-button
+    const navBtn = document.querySelector('.nav-button');
+    const navLinks = document.querySelector(".nav-links");
+
+    toggleNavLinks = () => {
+        navLinks.classList.toggle('close')
+    }
+
+    navBtn.addEventListener('click', toggleNavLinks)
+    // END OF toggle nav-links when press nav-button
+
+
+
+    //  listener for resizeing window to remove close class from nav-link
+    normalizeNavLink = () => {
+        let windowWidth = window.innerWidth;
+        if (windowWidth > 768) {
+            navLinks.classList.remove('close')
+        } else {
+            navLinks.classList.add('close')
+        }
+    }
+
+    window.addEventListener('resize', normalizeNavLink)
+    //   END OF listener for resizeing window to remove close class from nav-link
+
+
+
+
+
 });
