58f097a32cb9d801bacfe478475a2da19a56ac5d
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-11-06 18:03:45 +0100

    fix mobile navigation height. Blocked body when nav is open and when download modal is open

--- js/others.js
+++ js/others.js
@@ -9,14 +9,17 @@ const showBackToUpButton = () => {
     }
 }
 
+const body = document.querySelector('body')
 const toggleNavLinks = () => {
     const navBtn = document.querySelector('.nav-button');
     const navLinks = document.querySelector(".nav-links");
+    body.classList.toggle('blocked')
     navLinks.classList.toggle('open')
     navBtn.classList.toggle('open')
 }
 
 const closeNavLinks = () => {
+    body.classList.remove('blocked')
     const navLinks = document.querySelector('.nav-links')
     const navBtn = document.querySelector('.nav-button');
     let windowWidth = window.innerWidth;
